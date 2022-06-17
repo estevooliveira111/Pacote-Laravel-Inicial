a8b386a3a9376396ad3b75c8591716fd0b6dd20f
Estevão de Oliveira Nascimento <63885372+estevooliveira111@users.noreply.github.com>
2022-06-17 18:13:19 -0300

    ...

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,13 +10,17 @@
         <div class="mx-auto w-full max-w-sm lg:w-96">
 
             <div>
-                <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="">
+                <svg class="h-12 w-auto" xmlns="http://www.w3.org/2000/svg" fill="#3B82F6" viewBox="0 0 16 16">
+                    <path d="M0 2a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a2.5 2.5 0 0 1-2.5 2.5h-9A2.5 2.5 0 0 1 1 12.5V5a1 1 0 0 1-1-1V2zm2 3v7.5A1.5 1.5 0 0 0 3.5 14h9a1.5 1.5 0 0 0 1.5-1.5V5H2zm13-3H1v2h14V2zM5 7.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                </svg>
+                  
                 <h2 class="mt-6 text-3xl font-extrabold text-gray-900">Faça login em sua conta</h2>
             </div>
 
             <div class="mt-8">
                 
                 <div class="mt-6">
+
                     <!-- Session Status -->
                     <x-auth-session-status class="mb-4" :status="session('status')" />
 
